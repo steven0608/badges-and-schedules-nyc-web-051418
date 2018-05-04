782907a6c5b8f24ebbdf5b4de76fbe565b11f438
Steven Chen <ysteven.chen2@gmail.com>
2018-05-04 03:06:47 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,9 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-attendees.each {|x| "Hello, my name is #{x}."}
+attendees.each do |x|
+puts "Hello, my name is #{x}."
+end
 end
 
 def assign_rooms
